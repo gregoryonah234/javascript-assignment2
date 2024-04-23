53952a37941dd98f076fd6755a915257b7784f2e
gregoryonah234 <160475062+gregoryonah234@users.noreply.github.com>
2024-04-23 20:00:34 +0100

    Add files via upload
    THE TWO ASSIGNMENTS YOU GAVE ME THEY`RE UNDER THIS FILE ARRAY.JS

---
+++ array.js
@@ -0,0 +1,92 @@
+// const fruits =  ("mango","apple","bananas","watermelon")
+
+// // console.log(fruits.length);
+// // fruits.push("Orange");
+// // fruits.unshift("paw paw");
+
+
+// // let removeItems = fruits.toUpperCase();
+// // console.log(removeItems);
+// // letremoveFirst = fruits.shift();
+
+// // const number = (3, 6, 10, 2, 7, 5);
+
+// // function addition(odd){
+     
+// // }
+
+
+// const number = (1,3,1,6,3,2);
+
+// function removeDuplicate(arr){
+//     let result = [];
+//       for(let i = arr.length; i++){
+//         if(!is equal.include arr[i]){
+//             duplicate.push[arr]
+//         }
+//       }
+// }
+
+// console.log(removeDuplicate(num));
+
+
+
+
+// let peo = ["Mike", "Kenny"];
+// let pets = ["Cat","Dog"];
+
+// let combine = peo.concat(pets);
+// console.log(combine);
+
+// let newEll = [...peo, ...pets]
+// console.log(newEll);0
+
+
+// function missingNumber(array){
+//     let arraySum = 0
+//     for(let index = 0; index < array.length;index++){
+//         arraySum += array[index]
+//     }
+//     return 45 - arraySum
+// }
+
+
+
+
+
+
+
+// assignment
+
+// function convertCamelCase(str){
+//     let strArray = str.split(/[-_]/)
+
+//      for(let index = 0; index < strArray.length; index++){
+
+//         if (index == 0){
+//            continue
+//         }
+//         let currentString = strArray{index}
+//         let capChar = currentString[0].toYpperCase()
+//      }
+//       return strArray.join("")
+// }
+
+
+// console.log(convertCamelCase("the-stealth-warrior"));
+
+// assignment 2
+
+// function switchChar(inputChar, actualChar,changeChar){
+//    if(actualChar.length != changeChar.lengtn){
+//       return "Length mismatch."
+
+//    for (let index = 0; index < inputChar.length; index++){
+
+//       if(actualChar.indexOf(inputChar[index]) == -1) {
+//          return -1
+//       }
+//       outChar += changeChar[actualChar.indexOf(inputCharp[index])] 
+//    }
+//    return outChar
+// }
